7ac9618490b78f00d9a6a6efcfa2cb9b6ab45f12
Usama Mehmood <usamamehmood62000@gmail.com>
2022-04-12 23:47:36 -0700

    first commit

---
+++ MoviesCrudApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCrudApp.Models;
+
+namespace MoviesCrudApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MoviesCrudApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviesCrudApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
